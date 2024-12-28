8407e551f525a4396037dcaf9396757a48590ff0
Dmitriy <homyakov-dmitriy@mail.ru>
2024-12-28 18:25:40 +0200

    Deleting unnecessary variables, changing some types of variables and constructor, correcting documentation

--- Core/Models/TaskCategory.cs
+++ Core/Models/TaskCategory.cs
@@ -5,16 +5,15 @@
     /// </summary>
     public class TaskCategory
     {
-        private int id;
+        private Guid id;
         private string name;
-        private bool isHabit;
-        private int? userId;
-        private User user;
+        private Guid userId;
+        private User? user;
 
         /// <summary>
         /// Gets the unique identifier of the task category.
         /// </summary>
-        public int Id => id;
+        public Guid Id => id;
 
         /// <summary>
         /// Gets or sets the name of the task category.
@@ -22,63 +21,39 @@
         /// <exception cref="ArgumentNullException">Thrown if the task name is null or empty.</exception>
         public string Name
         {
-            get => name;
+            get { return name; }
             set
             {
-                if(string.IsNullOrEmpty(name))
+                if (string.IsNullOrEmpty(name))
                     throw new ArgumentNullException(nameof(name), "The name of the task category can not be null or empty");
                 name = value;
             }
         }
 
         /// <summary>
-        /// Gets or sets a value, is it a habit.
+        /// Gets the ID of the user assigned to the task category.
         /// </summary>
-        public bool IsHabit
-        {
-            get => isHabit;
-            set => isHabit = value;
-        }
-
-        /// <summary>
-        /// Gets or sets the ID of the user assigned to the task category.
-        /// </summary>
-        public int? UserId
-        {
-            get => userId;
-            set => userId = value;
-        }
+        public Guid UserId => userId;
 
         /// <summary>
         /// Gets or sets the User assigned to the task category.
         /// </summary>
-        /// <exception cref="ArgumentNullException">Thrown if the User is null.</exception>
-        public User User
+        public User? User
         {
-            get => user;
-            set
-            {
-                if (value == null)
-                    throw new ArgumentNullException(nameof(user), "The task category must be linked to the user");
-                user = value;
-            }
+            get { return user; }
+            set { user = value; }
         }
 
         /// <summary>
         /// Initialize a new instance of the <see cref="TaskCategory"/> class with the specified parameters.
         /// </summary>
-        /// <param name="id">The unique identifier of the task category.</param>
         /// <param name="name">The name of the task category.</param>
-        /// <param name="isHabit">An idicator of whether it is a habit.</param>
         /// <param name="userId">The ID of the user assigned to the task category.</param>
-        /// <param name="user">The user assigned to the task category.</param>
-        public TaskCategory(int id, string name, bool isHabit, int? userId, User user)
+        public TaskCategory(string name, Guid userId)
         {
-            this.id = id;
-            Name = name;
-            IsHabit = isHabit;
-            UserId = userId;
-            User = user;
+            id = new Guid();
+            this.name = name;
+            this.userId = userId;
         }
     }
 }
