57b0fa986c6e547496acf2cccb7b866486b33a0b
Dmitriy <homyakov-dmitriy@mail.ru>
2025-01-03 14:22:26 +0200

    Correcting int to guid, adding new method GetTaskCategoryById, correcting documentation

--- Core/Interfaces/ITaskCategoryService.cs
+++ Core/Interfaces/ITaskCategoryService.cs
@@ -8,28 +8,35 @@ namespace Core.Interfaces
     internal interface ITaskCategoryService
     {
         /// <summary>
+        /// Retrieves a task category by its unique ID.
+        /// </summary>
+        /// <param name="taskCategoryId">The unique identifier of the task category.</param>
+        /// <returns></returns>
+        TaskCategory GetTaskCategoryById(Guid taskCategoryId);
+
+        /// <summary>
         /// Retrieves a list of task categories associated with a specific user.
         /// </summary>
         /// <param name="userId">The unique identifier of the user.</param>
         /// <returns>A list of task categores belonging to the specified user.</returns>
-        List<TaskCategory> GetCategoriesByUser(int userId);
+        List<TaskCategory> GetCategoriesByUser(Guid userId);
 
         /// <summary>
         /// Adds a new task category to the system.
         /// </summary>
-        /// <param name="category">The task category to be added.</param>
-        void AddCategory(TaskCategory category);
+        /// <param name="taskCategory">The task category to be added.</param>
+        void AddCategory(TaskCategory taskCategory);
 
         /// <summary>
         /// Updates an existing task category in the system.
         /// </summary>
-        /// <param name="category">The task category with updated information.</param>
-        void UpdateCategory(TaskCategory category);
+        /// <param name="taskCategory">The task category with updated information.</param>
+        void UpdateCategory(TaskCategory taskCategory);
 
         /// <summary>
         /// Deletes a task category by its unique identifier.
         /// </summary>
-        /// <param name="id">The unique identifier of the task category to be deleted.</param>
-        void DeleteCategory(int id);
+        /// <param name="taskCategoryId">The unique identifier of the task category to be deleted.</param>
+        void DeleteCategory(Guid taskCategoryId);
     }
 }
