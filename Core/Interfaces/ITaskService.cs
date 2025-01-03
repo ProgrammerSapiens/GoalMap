3db60caf2a0ef9f22dc9fd3f5f674e824a91e660
Dmitriy <homyakov-dmitriy@mail.ru>
2025-01-03 14:17:27 +0200

    Adding new methods MoveRepeatedTasks and GetTasksByTimeBlock, correcting int to guid

--- Core/Interfaces/ITaskService.cs
+++ Core/Interfaces/ITaskService.cs
@@ -1,4 +1,7 @@
-﻿namespace Core.Interfaces
+﻿using Core.Models;
+using Task = Core.Models.Task;
+
+namespace Core.Interfaces
 {
     /// <summary>
     /// Provides methods for managing tasks, including CRUD operations and task movement.
@@ -10,14 +13,14 @@
         /// </summary>
         /// <param name="id">The unique identifier of the task.</param>
         /// <returns>The task with the specified identifier.</returns>
-        Task GetTaskById(int id);
+        Task GetTaskById(Guid id);
 
         /// <summary>
         /// Retrieves a list of tasks associated with a specific user.
         /// </summary>
         /// <param name="userId">The unique identifier of the user.</param>
         /// <returns>A list of tasks assigned to the specified user.</returns>
-        List<Task> GetTasksByUser(int userId);
+        List<Task> GetTasksByUser(Guid userId);
 
         /// <summary>
         /// Retrieves a list of tasks for a specific user and date.
@@ -25,7 +28,15 @@
         /// <param name="userId">The unique identifier of the user.</param>
         /// <param name="date">The date for which tasks are to be retrieved.</param>
         /// <returns>A list of tasks scheduled for the specified user on the given date.</returns>
-        List<Task> GetTasksByDate(int userId, DateTime date);
+        List<Task> GetTasksByDate(Guid userId, DateTime date);
+
+        /// <summary>
+        /// Retrieves a list of tasks for a specific user and date.
+        /// </summary>
+        /// <param name="userId">The unique identifier of the user.</param>
+        /// <param name="timeBlock">The time block to which the task belongs.</param>
+        /// <returns>A list of tasks scheduled for the specified user on the given date.</returns>
+        List<Task> GetTasksByTimeBlock(Guid userId, TimeBlock timeBlock);
 
         /// <summary>
         /// Adds a new task to the system.
@@ -43,7 +54,7 @@
         /// Deletes a task from the system by its unique identifier.
         /// </summary>
         /// <param name="id">The unique identifier of the task to be deleted.</param>
-        void DeleteTask(int id);
+        void DeleteTask(Guid id);
 
         /// <summary>
         /// Moves incomplete tasks from one date to another for a specified user.
@@ -51,6 +62,13 @@
         /// <param name="fromDate">The date from which tasks will be moved.</param>
         /// <param name="toDate">The date to which tasks will be moved.</param>
         /// <param name="userId">The unique identifier of the user whose tasks are being moved.</param>
-        void MoveIncompleteTasks(DateTime fromDate, DateTime toDate, int userId);
+        void MoveIncompleteTasks(DateTime fromDate, DateTime toDate, Guid userId);
+
+        /// <summary>
+        /// Moves incomplete tasks from one date to another for a specified user.
+        /// </summary>
+        /// <param name="repeatFrequency">The repeat frequency of the task.</param>
+        /// <param name="userId">The unique identifier of the user whose tasks are being moved.</param>
+        void MoveRepeatedTasks(RepeatFrequency repeatFrequency, Guid userId);
     }
 }
