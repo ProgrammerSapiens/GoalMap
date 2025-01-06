31ee49c66ca9713c0f69400939ac7039ec116892
Dmitriy <homyakov-dmitriy@mail.ru>
2025-01-06 21:06:25 +0200

    Creating an interface for creating and verifying a password

---
+++ Core/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.Interfaces
+{
+    /// <summary>
+    /// Provides methods for hashing passwords and verifying hashed passwords.
+    /// </summary>
+    public interface IPasswordHasher
+    {
+        /// <summary>
+        /// Hashes a plain text password and returns the hashed result.
+        /// </summary>
+        /// <param name="password">The plain text password.</param>
+        /// <returns>The hashed password.</returns>
+        string HashPassword(string password);
+
+        /// <summary>
+        /// Verifies if the provided password matches the stored hashed password.
+        /// </summary>
+        /// <param name="password">The plain text password.</param>
+        /// <param name="hashedPassword">The stored hashed password.</param>
+        /// <returns>
+        /// <c>true</c> if the password matches; otherwise, <c>false</c>
+        /// </returns>
+        bool VerifyPassword(string password, string hashedPassword);
+    }
+}
