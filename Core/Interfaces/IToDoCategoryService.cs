dc4b9d55a620ee9d22987e54001cb6894af9496e
Dmitriy <homyakov-dmitriy@mail.ru>
2025-01-09 21:32:55 +0200

    Changing the order of arguments in methods

--- Core/Interfaces/IToDoCategoryService.cs
+++ Core/Interfaces/IToDoCategoryService.cs
@@ -13,7 +13,7 @@ namespace Core.Interfaces
         /// <param name="toDoCategoryName">The name of the ToDo category.</param>
         /// <param name="userId">The unique identifier of the user.</param>
         /// <returns>The ToDo category associated with the specified name and user ID.</returns>
-        Task<ToDoCategory> GetToDoCategoryByCategoryNameAsync(string toDoCategoryName, Guid userId);
+        Task<ToDoCategory> GetToDoCategoryByCategoryNameAsync(Guid userId, string toDoCategoryName);
 
         /// <summary>
         /// Retrieves a list of ToDo categories associated with a specific user.
@@ -39,6 +39,6 @@ namespace Core.Interfaces
         /// </summary>
         /// <param name="toDoCategoryName">The name of the ToDo category to be deleted.</param>
         /// <param name="userId">The unique identifier of the user.</param>
-        Task DeleteToDoCategoryAsync(string toDoCategoryName, Guid userId);
+        Task DeleteToDoCategoryAsync(Guid userId, string toDoCategoryName);
     }
 }
