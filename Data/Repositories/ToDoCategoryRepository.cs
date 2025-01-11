dc55675a720093e683db27d76b67507543aeb5f8
Dmitriy <homyakov-dmitriy@mail.ru>
2025-01-11 12:52:43 +0200

    Moving repositories to a new project

---
+++ Data/Repositories/ToDoCategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using Core.Interfaces;
+using Core.Models;
+
+namespace Data.Repositories
+{
+    public class ToDoCategoryRepository : IToDoCategoryRepository
+    {
+        public async Task<ToDoCategory> GetToDoCategoryByCategoryNameAsync(string toDoCategoryName, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ToDoCategory>> GetToDoCategoriesByUserIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddToDoCategoryAsync(ToDoCategory toDoCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateToDoCategoryAsync(ToDoCategory toDoCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteToDoCategoryAsync(string toDoCategoryName, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> IsCategoryExistsAsync(string toDoCategoryName, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Core.Interfaces;
+using Core.Models;
+
+namespace Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public async Task<User?> GetUserByUserNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> IsUserExistsAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
