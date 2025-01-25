3317052150d0fc69211dc7935a5dcc002f945945
Dmitriy <homyakov-dmitriy@mail.ru>
2025-01-25 19:44:19 +0200

    Adding services and repositories to the API configuration

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+using Core;
+using Core.Interfaces;
+using Core.Services;
+using Data.Repositories;
 
 namespace API
 {
@@ -10,6 +14,16 @@ namespace API
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IToDoCategoryRepository, ToDoCategoryRepository>();
+            builder.Services.AddScoped<IToDoRepository, ToDoRepository>();
+
+            builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IToDoCategoryService, ToDoCategoryService>();
+            builder.Services.AddScoped<IToDoService, ToDoService>();
 
             var app = builder.Build();
 
