68b5cfbc49548c0a1f56ef0dbce0b774b6b287c4
Dmitriy <homyakov-dmitriy@mail.ru>
2025-01-25 19:54:52 +0200

    Creating a comfiguration file for the API

--- Tests/IntegrationTests/APITests/CustomWebApplicationFactory.cs
+++ Tests/IntegrationTests/APITests/CustomWebApplicationFactory.cs
@@ -1,14 +1,32 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Core.Interfaces;
+using Data.DBContext;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Data.Repositories;
+using Core.Services;
+using Core;
 
 namespace Tests.IntegrationTests.APITests
 {
-    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    public class CustomWebApplicationFactory<Program> : WebApplicationFactory<Program> where Program : class
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.UseSetting("IsTesting", "true");
-            builder.UseStartup(typeof(TProgram));
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDb");
+                });
+            });
         }
     }
 }
