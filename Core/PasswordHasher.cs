a2408a4c38cad40021563243016114b4947616af
Dmitriy <homyakov-dmitriy@mail.ru>
2025-01-25 19:46:29 +0200

    Adding an empty class with interface implementation

---
+++ Core/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public Task<string> HashPasswordAsync(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> VerifyPasswordAsync(string password, string hashedPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
