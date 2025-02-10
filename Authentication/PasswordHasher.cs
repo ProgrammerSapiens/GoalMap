0b5cfb6296f8e7746c261e0641ba4369f8e7edfc
Dmitriy <homyakov-dmitriy@mail.ru>
2025-02-10 20:24:34 +0200

    Moving and implementing a password hashing class

---
+++ Authentication/PasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿using Core.Interfaces;
+
+namespace Authentication
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public Task<string> HashPasswordAsync(string password)
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            return Task.FromResult(hashedPassword);
+        }
+
+        public Task<bool> VerifyPasswordAsync(string password, string hashedPassword)
+        {
+            bool isVerified = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+            return Task.FromResult(isVerified);
+        }
+    }
+}
