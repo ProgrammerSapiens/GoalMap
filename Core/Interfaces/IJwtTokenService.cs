5c9ce72b3ca559ed0e0d56465656b7fdea64cb0f
Dmitriy <homyakov-dmitriy@mail.ru>
2025-02-11 13:25:00 +0200

    Adding documentation

--- Core/Interfaces/IJwtTokenService.cs
+++ Core/Interfaces/IJwtTokenService.cs
@@ -2,8 +2,16 @@
 
 namespace Core.Interfaces
 {
+    /// <summary>
+    /// Interface for services that handle JWT token generation.
+    /// </summary>
     public interface IJwtTokenService
     {
+        /// <summary>
+        /// Generates a JWT token for the specified user.
+        /// </summary>
+        /// <param name="user">The user for whom the JWT token will be generated.</param>
+        /// <returns>A task representing the asynchronous operation. The task result contains the generated JWT token as a string.</returns>
         Task<string> GenerateTokenAsync(User user);
     }
 }
