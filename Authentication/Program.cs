9e65957560aee0add9f9c7213bc6c5cf1aa27733
Dmitriy <homyakov-dmitriy@mail.ru>
2025-02-11 13:24:24 +0200

    Removing unnecessary stuff

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -1,7 +1,4 @@
 using Core.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace Authentication
 {
@@ -11,26 +8,10 @@ namespace Authentication
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers();
-
             builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
             builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
 
             builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                    ValidAudience = builder.Configuration["Jwt:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-                };
-            });
 
             builder.Services.AddAuthorization();
 
@@ -39,8 +20,8 @@ namespace Authentication
                 options.AddPolicy("AllowAll", policy =>
                 {
                     policy.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader();
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
                 });
             });
 
@@ -52,7 +33,6 @@ namespace Authentication
 
             app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
