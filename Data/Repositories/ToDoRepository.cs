6e0e5ed6d01303735a80cbb22916116b2253572a
Dmitriy <homyakov-dmitriy@mail.ru>
2025-02-14 19:02:46 +0200

    Adding logging

--- Data/Repositories/ToDoRepository.cs
+++ Data/Repositories/ToDoRepository.cs
@@ -2,6 +2,7 @@
 using Core.Models;
 using Data.DBContext;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Data.Repositories
 {
@@ -12,14 +13,16 @@ namespace Data.Repositories
     public class ToDoRepository : IToDoRepository
     {
         private readonly AppDbContext _context;
+        private readonly ILogger<ToDoRepository> _logger;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ToDoRepository"/> class.
         /// </summary>
         /// <param name="context">The application's database context.</param>
-        public ToDoRepository(AppDbContext context)
+        public ToDoRepository(AppDbContext context, ILogger<ToDoRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         /// <summary>
@@ -29,6 +32,8 @@ namespace Data.Repositories
         /// <returns>A task representing the asynchronous operation, containing the ToDo item if found; otherwise, <c>null</c>.</returns>
         public async Task<ToDo?> GetToDoByIdAsync(Guid toDoId)
         {
+            _logger.LogInformation($"GetToDoByIdAsync({toDoId})");
+
             return await _context.ToDos.AsNoTracking().SingleOrDefaultAsync(t => t.ToDoId == toDoId);
         }
 
@@ -41,6 +46,8 @@ namespace Data.Repositories
         /// <returns>A task representing the asynchronous operation, containing a list of ToDo items matching the specified criteria.</returns>
         public async Task<List<ToDo>> GetToDosAsync(Guid userId, DateTime date, TimeBlock timeBlock)
         {
+            _logger.LogInformation($"GetToDosAsync({userId}, {date}, {timeBlock})");
+
             return await _context.ToDos.AsNoTracking().Where(t => t.UserId == userId && t.ToDoDate.Date == date.Date && t.TimeBlock == timeBlock).ToListAsync();
         }
 
@@ -52,8 +59,11 @@ namespace Data.Repositories
         /// <exception cref="InvalidOperationException">Thrown if a ToDo item with the same Id already exists.</exception>
         public async Task AddToDoAsync(ToDo toDo)
         {
+            _logger.LogInformation($"AddToDoAsync({toDo.Description})");
+
             if (await ToDoExistsAsync(toDo.ToDoId))
             {
+                _logger.LogWarning($"ToDo {toDo.Description} already exists.");
                 throw new InvalidOperationException("Todo already exists.");
             }
 
@@ -69,10 +79,13 @@ namespace Data.Repositories
         /// <exception cref="InvalidOperationException">Thrown if the ToDo item does not exist.</exception>
         public async Task UpdateToDoAsync(ToDo toDo)
         {
+            _logger.LogInformation($"UpdateToDoAsync({toDo.Description})");
+
             var existingToDo = await _context.ToDos.SingleOrDefaultAsync(t => t.ToDoId == toDo.ToDoId);
 
             if (existingToDo == null)
             {
+                _logger.LogWarning($"ToDo {toDo.Description} does not exists.");
                 throw new InvalidOperationException("ToDo with such an Id does not exists.");
             }
 
@@ -89,10 +102,13 @@ namespace Data.Repositories
         /// <exception cref="InvalidOperationException">Thrown if the ToDo item does not exist.</exception>
         public async Task DeleteToDoAsync(Guid toDoId)
         {
+            _logger.LogInformation($"DeleteToDoAsync({toDoId})");
+
             var existingToDo = _context.ToDos.FirstOrDefault(t => t.ToDoId == toDoId);
 
             if (existingToDo == null)
             {
+                _logger.LogWarning($"ToDo with id {toDoId} does not exist.");
                 throw new InvalidOperationException("Todo id does not exist.");
             }
 
@@ -107,6 +123,8 @@ namespace Data.Repositories
         /// <returns>A task representing the asynchronous operation, returning <c>true</c> if the ToDo item exists; otherwise, <c>false</c>.</returns>
         public async Task<bool> ToDoExistsAsync(Guid toDoId)
         {
+            _logger.LogInformation($"ToDoExistsAsync({toDoId})");
+
             return await _context.ToDos.AsNoTracking().AnyAsync(t => t.ToDoId == toDoId);
         }
     }
--- Data/Repositories/UserRepository.cs
+++ Data/Repositories/UserRepository.cs
@@ -2,6 +2,7 @@
 using Core.Models;
 using Data.DBContext;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Data.Repositories
 {
@@ -12,14 +13,16 @@ namespace Data.Repositories
     public class UserRepository : IUserRepository
     {
         private readonly AppDbContext _context;
+        private readonly ILogger<UserRepository> _logger;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="UserRepository"/> class with the specified database context.
         /// </summary>
         /// <param name="context">The database context used to interact with the data store.</param>
-        public UserRepository(AppDbContext context)
+        public UserRepository(AppDbContext context, ILogger<UserRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         /// <summary>
@@ -29,6 +32,8 @@ namespace Data.Repositories
         /// <returns>A task that represents the asynchronous operation. The task result contains the user if found, or null otherwise.</returns>
         public async Task<User?> GetUserByUserIdAsync(Guid userId)
         {
+            _logger.LogInformation($"GetUserByUserIdAsync({userId})");
+
             return await _context.Users.AsNoTracking().SingleOrDefaultAsync(u => u.UserId == userId);
         }
 
@@ -39,6 +44,8 @@ namespace Data.Repositories
         /// <returns>A task that represents the asynchronous operation. The task result contains the user if found, or null otherwise.</returns>
         public async Task<User?> GetUserByUserNameAsync(string userName)
         {
+            _logger.LogInformation($"GetUserByUserNameAsync({userName})");
+
             return await _context.Users.AsNoTracking().SingleOrDefaultAsync(u => u.UserName == userName);
         }
 
@@ -50,9 +57,12 @@ namespace Data.Repositories
         /// <exception cref="InvalidOperationException">Thrown if a user with the same username already exists.</exception>
         public async Task AddUserAsync(User user)
         {
+            _logger.LogInformation($"AddUserAsync({user.UserName})");
+
             var userExists = await GetUserByUserNameAsync(user.UserName);
             if (userExists != null)
             {
+                _logger.LogWarning($"User {user.UserName} already exists.");
                 throw new InvalidOperationException("Username already exists.");
             }
 
@@ -68,10 +78,13 @@ namespace Data.Repositories
         /// <exception cref="InvalidOperationException">Thrown if the user does not exist in the database.</exception>
         public async Task UpdateUserAsync(User user)
         {
+            _logger.LogInformation($"UpdateUserAsync({user.UserName})");
+
             var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.UserId == user.UserId);
 
             if (existingUser == null)
             {
+                _logger.LogWarning($"User {user.UserName} was not found.");
                 throw new InvalidOperationException("User not found.");
             }
 
@@ -87,6 +100,8 @@ namespace Data.Repositories
         /// <returns>A task that represents the asynchronous operation.</returns>
         public async Task AddDefaultCategoriesAsync(List<ToDoCategory> defaultCategories)
         {
+            _logger.LogInformation($"AddDefaultCategoriesAsync(List<ToDoCategory defaultCategories(Other, Habbit))");
+
             await _context.ToDoCategories.AddRangeAsync(defaultCategories);
             await _context.SaveChangesAsync();
         }
