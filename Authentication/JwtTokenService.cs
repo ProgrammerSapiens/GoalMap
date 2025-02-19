b6d43cf7227ad40bfc6dc7d2d262349a0b85dbca
Dmitriy <homyakov-dmitriy@mail.ru>
2025-02-19 22:07:02 +0200

    Change of the token validity period until the end of the current day

--- Authentication/JwtTokenService.cs
+++ Authentication/JwtTokenService.cs
@@ -46,13 +46,15 @@ namespace Authentication
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
             };
 
+            var expiration = DateTime.UtcNow.Date.AddDays(1).AddTicks(-1);
+
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
             var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
             var token = new JwtSecurityToken(
                 issuer: _configuration["Jwt:Issuer"],
                 audience: _configuration["Jwt:Audience"],
                 claims: claims,
-                expires: DateTime.UtcNow.AddDays(1),
+                expires: expiration,
                 signingCredentials: credentials
             );
 
