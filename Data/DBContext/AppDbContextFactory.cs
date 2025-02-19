9f06b4a593aaa8d725e6f8456e14081c207c70e7
Dmitriy <homyakov-dmitriy@mail.ru>
2025-02-19 22:07:52 +0200

    The factory commiting out

--- Data/DBContext/AppDbContextFactory.cs
+++ Data/DBContext/AppDbContextFactory.cs
@@ -4,21 +4,21 @@ using Microsoft.Extensions.Configuration;
 
 namespace Data.DBContext
 {
-    //public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
-    //{
-    //    public AppDbContext CreateDbContext(string[] args)
-    //    {
-    //        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
 
-    //        var configuration = new ConfigurationBuilder()
-    //            .SetBasePath(Directory.GetCurrentDirectory())
-    //            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    //            .Build();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
 
-    //        var connectionString = configuration.GetConnectionString("DefaultConnection");
-    //        optionsBuilder.UseSqlServer(connectionString);
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
 
-    //        return new AppDbContext(optionsBuilder.Options);
-    //    }
-    //}
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
 }
