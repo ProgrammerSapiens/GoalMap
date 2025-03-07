d42c919b238fec07fc09bb190d4ed8da23ef06a7
Dmitriy <homyakov-dmitriy@mail.ru>
2025-03-07 19:06:00 +0200

    Adding a connection string for migration

--- Data/DBContext/AppDbContextFactory.cs
+++ Data/DBContext/AppDbContextFactory.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
 
 namespace Data.DBContext
 {
@@ -10,13 +9,7 @@ namespace Data.DBContext
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
 
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.UseSqlServer("Server=KOMPUTER\\SQLEXPRESS;Database=GoalMapDatabase;User Id=GoalMapUser;Password=StrongPassword123;TrustServerCertificate=True;");
 
             return new AppDbContext(optionsBuilder.Options);
         }
