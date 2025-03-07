de733a4622118df5a38528f997eda15e52e5475d
Dmitriy <homyakov-dmitriy@mail.ru>
2025-03-07 19:03:00 +0200

    Separation of addition and update models

---
+++ Core/DTOs/ToDoCategory/CategoryAddDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.DTOs.ToDoCategory
+{
+    /// <summary>
+    /// Represents the data required to add or update a To-Do category.
+    /// </summary>
+    public class CategoryAddDto
+    {
+        /// <summary>
+        /// The name of the To-Do category.
+        /// </summary>
+        public string? ToDoCategoryName { get; set; } = null;
+    }
+}
---
+++ Core/DTOs/ToDoCategory/CategoryUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.DTOs.ToDoCategory
+{
+    public class CategoryUpdateDto
+    {
+        /// <summary>
+        /// The name of the To-Do category.
+        /// </summary>
+        public string? ToDoCategoryName { get; set; } = null;
+
+        public Guid ToDoCategoryId { get; set; } = new Guid();
+    }
+}
